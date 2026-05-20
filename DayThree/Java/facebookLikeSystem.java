57295154c18fa566dfd372c359f96e4913e51b18
Ryan <ariyoquadri4@gmail.com>
2026-05-20 13:15:26 +0100

    Phase Gate

--- DayThree/Java/facebookLikeSystem.java
+++ DayThree/Java/facebookLikeSystem.java
@@ -18,9 +18,10 @@ public class facebookLikeSystem{
             return (names[0] + ", " + names[1] + " and " + names[2] + " like this");
         }
         else if(names.length == 4){
-            int remaining = names.length - 2;
+            int remaining = names.length - 2
             return (names[0] + ", " + names[1] + " and " + remaining + " others like this");
         }
         }
+        return "no one likes this";
     }
 }
